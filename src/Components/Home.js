ca63d3a8602c2ff34ed47a601c72ea7adbeef472
Gelo Yu <bbirds94@gmail.com>
2023-02-27 15:58:43 +0900

    [수정] 전체 레이아웃 css 수정

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "../Styles/css/Contents.css";
-import image from "../Styles/images/back.gif";
 
 function Home() {
   return (
     <>
       <div className="home_content">
-        <img className="home_image" src={image} alt="profile"></img>
         <p className="home_subtitle">
           <i style={{ fontSize: "50px" }}>Tech Record by Gelo</i>
           <br />
